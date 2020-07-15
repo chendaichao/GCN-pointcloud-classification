35d447b0c024135d21941cc551f7f27e4d708946
Daichao Chen <67040079+chendaichao@users.noreply.github.com>
2020-07-15 14:30:36 +0800

    Add dense connections for GCN layers.

--- Models/PointNet+GCN/model.py
+++ Models/PointNet+GCN/model.py
@@ -7,18 +7,16 @@ from layers import GraphConvolution
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 class GCN(nn.Module):
-    '''
-      part of the codes are borrowed from github.com/tkipf/pygcn/blob/master/pygcn/model.py
-    '''
-    def __init__(self, n_in, n_hid, n_out, dropout=0):
+    def __init__(self, n_in, n_hid1, n_hid2, n_out):
         super(GCN, self).__init__()
 
-        self.gc1 = GraphConvolution(n_in, n_hid)
-        self.batchnorm1 = BatchNorm(n_hid)
-        self.gc2 = GraphConvolution(n_hid, n_out)
-        self.batchnorm2 = BatchNorm(n_out)
-        self.dropout = nn.Dropout(dropout)
-
+        self.gc1 = GraphConvolution(n_in, n_hid1)
+        self.batchnorm1 = BatchNorm(n_hid1)
+        self.gc2 = GraphConvolution(n_in+n_hid1, n_hid2)
+        self.batchnorm2 = BatchNorm(n_hid2)
+        self.gc3 = GraphConvolution(n_in+n_hid1+n_hid2, n_out)
+        self.batchnorm3 = BatchNorm(n_out)
+    
     def forward(self, xs, adjs = None):
         if (adjs is None):
             xs, adjs = xs
@@ -29,14 +27,16 @@ class GCN(nn.Module):
         xs = xs.to(device)
         adjs = adjs.to(device)
         
-        xs1 = F.relu(self.batchnorm1(self.gc1(xs, adjs)))
-        xs2 = F.relu(self.batchnorm2(self.gc2(xs1, adjs)))
+        xs1 = torch.cat( (xs, F.relu(self.batchnorm1(self.gc1(xs, adjs)))), dim = 1)
+        del xs
+        xs2 = torch.cat( (xs1, F.relu(self.batchnorm2(self.gc2(xs1, adjs)))), dim = 1)
+        del xs1
+        xs3 = torch.cat( (xs2, F.relu(self.batchnorm3(self.gc3(xs2, adjs)))), dim = 1)
+        del xs2
         
-        res = torch.cat((xs, xs1, xs2), dim=1)
-        del xs, xs1, xs2
-        
+        res = xs3
         ys = torch.stack(torch.split(res, num_points, dim=0)).to(device)
-        return self.dropout(ys)
+        return ys
 
 class MaxPooling(nn.Module):
     def __init__(self):
@@ -55,7 +55,8 @@ class Permute(nn.Module):
 class BatchNorm(nn.Module):
     '''
         Perform batch normalization.
-        Input: A tensor of size (N, M, feature_dim), or (N, feature_dim, M) (available when feature_dim != M), 
+        Input: A tensor of size (N, M, feature_dim), or (N, feature_dim, M) 
+                                               ( it would be the former case if feature_dim == M ), 
                 or (N, feature_dim)
         Output: A tensor of the same size as input.
     '''
@@ -78,7 +79,7 @@ class MLP(nn.Module):
             in_dim = hidden_size[i]
             out_dim = hidden_size[i+1]
             q.append(("Linear_%d" % i, nn.Linear(in_dim, out_dim)))
-            if (i == len(hidden_size) - 2) and (last_activation):
+            if (i < len(hidden_size) - 2) or ((i == len(hidden_size) - 2) and (last_activation)):
                 if (batchnorm):
                     q.append(("Batchnorm_%d" % i, BatchNorm(out_dim)))
                 q.append(("ReLU_%d" % i, nn.ReLU(inplace=True)))
@@ -87,12 +88,13 @@ class MLP(nn.Module):
         return self.mlp(x)
     
 class TNet(nn.Module):
-    def __init__(self, nfeat):
+    def __init__(self, nfeat, dropout = 0):
         super(TNet, self).__init__()
         self.nfeat = nfeat
-        self.encoder = MLP((nfeat, 64, 512))
-        self.gcn = GCN(512, 512, 1024)
-        self.decoder = nn.Sequential(MaxPooling(), BatchNorm(2048), MLP((2048, 512, 256, nfeat*nfeat)))
+        self.encoder = MLP((nfeat, 64, 128, 128))
+        self.gcn = GCN(128, 128, 256, 512)
+        self.decoder = nn.Sequential(MaxPooling(), BatchNorm(1024), 
+                                     MLP((1024, 512, 256)), nn.Dropout(dropout), MLP((256, nfeat*nfeat)))
         
     def forward(self, x, adjs):
         batch_size = x.shape[0]
@@ -103,14 +105,14 @@ class PointNetGCN(nn.Module):
     def __init__(self, nfeat, nclass, dropout = 0):
         super(PointNetGCN, self).__init__()
 
-        self.input_transform = TNet(nfeat)
+        self.input_transform = TNet(nfeat, 0.1)
         self.encoder = nn.Sequential(BatchNorm(3), MLP((nfeat, 64, 64)))
-        self.feature_transform = TNet(64)
+        self.feature_transform = TNet(64, 0.1)
         self.batchnorm = BatchNorm(64)
-        self.mlp = MLP((64, 128, 512))
-        self.gcn = GCN(512, 512, 1024)
+        self.mlp = MLP((64, 64, 128, 128))
+        self.gcn = GCN(128, 128, 256, 512)
         self.maxpooling = MaxPooling()
-        self.decoder = nn.Sequential(BatchNorm(2048), MLP((2048, 512, 256)), nn.Dropout(dropout), nn.Linear(256, nclass))
+        self.decoder = nn.Sequential(BatchNorm(1024), MLP((1024, 512, 256)), nn.Dropout(dropout), nn.Linear(256, nclass))
 
         self.eye64 = torch.eye(64).to(device)
         
@@ -124,7 +126,7 @@ class PointNetGCN(nn.Module):
         transform = self.feature_transform(xs, adjs)
         xs = torch.stack([torch.mm(xs[i],transform[i]) for i in range(batch_size)])
         
-        xs = self.gcn(self.mlp(self.batchnorm(xs)), adj)
+        xs = self.gcn(self.mlp(self.batchnorm(xs)), adjs)
         xs = self.decoder(self.maxpooling(xs))
         
         if (self.training):
